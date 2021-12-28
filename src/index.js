d2ddbce3397b7a2aed315363db238f643bded3f4
Jay2009 <jaem2009@naver.com>
2021-12-28 16:04:21 +0900

    Vanilla redux--todo list

--- src/index.js
+++ src/index.js
@@ -1,39 +1,78 @@
 import {createStore} from "redux";
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+// we make a fixed variable not to make typo when coding
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-number.innerText = 0;
+// we put only actions here. 
+const addToDo = text => {
+    return {
+        type: ADD_TODO,
+        text
+    };
+}
 
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-const countModifier = (count = 0, action) => {
+const deleteToDo = id => {
+    return {
+        type: DELETE_TODO,
+        id
+    };
+}
+// reducer is a nessasery when using redux, only in reducer function we change data. and the return value will be a state or action in the function and state should not be mutated . so we use spread function
+const reducer = (state = [], action) => {
+    
     switch (action.type){
-        case ADD:
-            return count + 1;
-        case MINUS:
-            return count - 1;
+        case ADD_TODO:
+            return [ {text: action.text, id: Date.now() }, ...state];
+        case DELETE_TODO:
+            return state.filter(toDo => toDo.id !== action.id); // currently clicked element is not same as all the elements in the array  then create a new array without the clicked element.
         default:
-            return count;
+            return state;
     }
 };
+// this is where we store the data
+const store = createStore(reducer);
 
-const countStore = createStore(countModifier);
+// If wants to listen the change we use subscribe.
+store.subscribe(()=> console.log(store.getState()));
 
-const onChange = () => {
-    number.innerText = countStore.getState();
+// this is where we call the action using dispatch.
+const dispatchAddToDo = text => {
+    store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = e => {
+    const id= parseInt(e.target.parentNode.id);
+    store.dispatch(deleteToDo(id));
+};
+
+
+const paintToDos = () => {
+    const toDos = store.getState();
+    ul.innerHTML="";
+    toDos.forEach(toDo => {
+        const li = document.createElement("li");
+        const btn = document.createElement("button");
+        btn.innerText="DEL";
+        btn.addEventListener("click",dispatchDeleteToDo)
+        li.id= toDo.id;
+        li.innerText=toDo.text;
+        li.appendChild(btn);
+        ul.appendChild(li);
+    });
 }
+// If wants to listen the change we use subscribe.
+store.subscribe(paintToDos);
 
-countStore.subscribe(onChange);
 
-const handleAdd = () => {
-    countStore.dispatch({type: ADD})
-}
 
-const handleMinus = () => {
-    countStore.dispatch({type: MINUS})
-}
+const onSubmit = e => {
+    e.preventDefault();
+    const toDo = input.value;
+    input.value = "";
+    dispatchAddToDo(toDo);
+};
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
